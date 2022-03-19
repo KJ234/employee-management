d3d029d7eb704e267ed5df3731397a034b8ed6a0
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-03-20 00:18:53 +0500

    adding functions

--- index.js
+++ index.js
@@ -20,29 +20,93 @@ db.connect((err) => {
   startApp();
 });
 
+//displays the following as a start of the app
+var startApp = () => {
+  console.log("***********************************");
+  console.log("*                                 *");
+  console.log("*        EMPLOYEE MANAGER         *");
+  console.log("*                                 *");
+  console.log("***********************************");
+  startInquirer();
+};
+//starts Inquirer
 var startInquirer = () => {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "choices",
-      message: "What would you like to do",
-      choices: [
-        "View All Departments",
-        "View All Roles",
-        "View All Employees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update an employee role",
-        "Update an employee manager",
-        "View employees by manager",
-        "View employees by department",
-        "Delete a department",
-        "Delete a role",
-        "Delete an employee",
-        "View department budgets",
-        "No action",
-      ],
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choices",
+        message: "What would you like to do",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Delete a department",
+          "Delete a role",
+          "Delete an employee",
+          "View department budgets",
+          "No action",
+        ],
+      },
+    ])
+    .then((answers) => {
+      const { choices } = answers;
+      // In case user chooses View all departments
+      if (choices == "View All Departments") {
+        showDepartments();
+      }
+      if (choices == "View All Roles") {
+        showRoles();
+      }
+      if (choices == "View All Employees") {
+        showAllEmployees();
+      }
+      if (choices == "Add a department") {
+        addAnotherDepartment();
+      }
+      if (choices == "Add a role") {
+        addRole();
+      }
+      if (choices == "Add an employee") {
+        addEmployee();
+      }
+      if (choices == "Update an employee role") {
+        updateRole();
+      }
+      if (choices === "Delete a department") {
+        deleteDepartment();
+      }
+      if (choices === "Delete a role") {
+        deleteRole();
+      }
+      if (choices === "Delete an employee") {
+        deleteEmployee();
+      }
+      if (choices === "View department budgets") {
+        viewBudget();
+      }
+      if (choices == "No action") {
+        db.end();
+      }
+    });
+};
+
+//Show Departments
+showDepartments = () => {
+  const sql = `SELECT 
+  department.id,
+  department.name
+  FROM
+  department
+  `;
+
+  db.query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    startInquirer();
+  });
 };
