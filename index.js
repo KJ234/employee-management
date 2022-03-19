fda2cf9cd735068b2e84e9e1e027e74190bdb576
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-03-19 17:27:37 +0500

    import i, mysql2, console.table

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-//starts Inquirer
+// import inquirer and express
+const inquirer = require("inquirer");
+// Import and require mysql2
+const mysql = require("mysql2");
+// import console.table
+const cTable = require("console.table");
+
+require("dotenv").config();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "employee_db",
+});
+//Check if there's an error, else starts the application
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to employee_db database");
+  startApp();
+});
+
 var startInquirer = () => {
   inquirer.prompt([
     {
